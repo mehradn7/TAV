06fc0d32fdbe346e9d1fffd1bf456e56ffee7eb3
mehrad rafii <mehrad.rafii@yahoo.fr>
2018-02-20 18:20:04 +0100

    tp3 ex2

--- TP3/simulation.m
+++ TP3/simulation.m
@@ -1,10 +1,9 @@
 function [x_gauche,x_droite] = simulation(y,beta_0,gamma_0,delta_moyen,sigma_delta,d)
-    beta = delta_moyen + sigma_delta.*randn(1,2*d-1);
-    beta = [beta(1:d-1), beta(2*d-1)];
+    delta = delta_moyen + sigma_delta.*randn(1,2*d-1);
+    beta = [delta(1:d-1), delta(2*d-1)];
     x_gauche = bezier(y,beta_0,beta);
 
-    gamma = delta_moyen + sigma_delta.*randn(1,2*d-1);
-    gamma = gamma(d:2*d-1);
+    gamma = delta(d:2*d-1);
     x_droite = bezier(y,gamma_0,gamma);
 
 end
