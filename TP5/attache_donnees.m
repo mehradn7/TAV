8e367a4c646b917c83c33fb4664739f42d2476a7
mehrad rafii <mehrad.rafii@yahoo.fr>
2018-03-19 22:09:30 +0100

    fin TP5

--- TP5/attache_donnees.m
+++ TP5/attache_donnees.m
@@ -1,5 +1,5 @@
 function [AD] = attache_donnees(I,moyennes,variances)
-AD = zeros(size(I,1), size(I,2), length(moyennes));
+AD = zeros(size(I,1), size(I,2), size(moyennes,2));
 
 for k=1:length(moyennes)
     AD(:,:,k) = 0.5*log(variances(k)) + 0.5*((I(:,:) - moyennes(k))/sqrt(variances(k))).^2;
--- TP5/attache_donnees_RVB.m
+++ TP5/attache_donnees_RVB.m
@@ -1,1 +1,19 @@
 function AD_RVB = attache_donnees_RVB(I,moyennes,variances_covariances)
+[m,n,~] = size(I);
+N = size(moyennes,2);
+
+AD_RVB = zeros(m, n, N);
+
+for i=1:N
+    det_sigma = sum(diag(variances_covariances(:,:,i)));    
+    
+    for j=1:m
+        for k=1:n
+            moy_reshaped = reshape(moyennes(:,i),1,1,3);
+            difference = I(j,k,:) - moy_reshaped;
+            difference = reshape(difference, 3,1);
+            
+            AD_RVB(j,k,i) = 0.5*log(det_sigma) + (0.5*difference' * (variances_covariances(:,:,i)\difference));
+        end
+    end
+end
